da2aa7e7e3b1058fe84397f8bc7a54b85e42f779
nikolakoco89 <nikolakoco89@gmail.com>
2014-01-30 11:40:41 +0100

    Dixed css and added proper images links

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -33,32 +33,3 @@ function EmployeesCtrl($scope, $http) {
     };
 }
 
-
-
-
-
-
-//employeesApp.controller('EmployeesCtrl', ['$scope', '$http',
-//    function ($scope, $http) {
-//        $http.get('employees/employees.json').success(function (data) {
-//            $scope.employees = data;
-//        }).error(function (data, status) {
-//                window.alert("NIKOLA");
-//            });
-//
-//        $scope.orderProp = 'age';
-//
-//        $scope.save = function ($scope, $http) {
-///*            $http.post('employees/employees.json', { firstName: '$scope.firstName', lastName: '$scope.lastName', photo: '$scope.photo' }).success(function (response) {
-//
-//            }).error(function () {
-//                    window.alert("NIKOLA");
-//                });*/
-//            $scope.employees = [{
-//                'firstName': 'Nikolche',
-//                'lastName': 'Kochovski',
-//                'photo' : 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/c0.0.160.160/p160x160/1119366_1553250848_1992939247_n.jpg'
-//            }];
-//            //$scope.employees.push({firstName: '$scope.firstName', lastName: '$scope.lastName', photo: '$scope.photo'});
-//        };
-//    }]);
